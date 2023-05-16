f813e93f0e5e9e67c8f591f9078a30c0e9f63347
Samuel Mensah <samuelmensqh@gmail.com>
2023-05-16 03:35:35 +0000

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        '16': '16rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
